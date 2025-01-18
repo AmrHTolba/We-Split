e0b926b012a0cdc0213f60e16b243a2787779589
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-01-18 12:41:32 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Amr El-Fiqi on 18/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
