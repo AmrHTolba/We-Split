13d63f1cdc0868579622cef0db2064dba479f3b4
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-01-19 21:37:55 +0200

    Fully functioning app

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -14,7 +14,18 @@ struct ContentView: View {
     @State var checkAmount = 0.0
     @State var numberOfPeople = 2
     @State var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount/100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     let tipList = [10, 15, 20, 25, 0]
     
     // MARK: - Body
@@ -26,6 +37,7 @@ struct ContentView: View {
                     // Using Locale to get user region currency
                     TextField ("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     Picker("Number of People: ",selection: $numberOfPeople ) {
                         ForEach(2..<99){
                             Text("\($0) People")
@@ -42,11 +54,18 @@ struct ContentView: View {
                     .pickerStyle(.segmented)
                 }
                 
-                Section {
-                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                Section("Total Per Person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
-            .navigationTitle("")
+            .navigationTitle("We Split")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
